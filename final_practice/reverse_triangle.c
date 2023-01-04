55e3de864b4dd90b51c10365c1a776f8d8cc692b
Cenk G <cenkkgl@gmail.com>
2023-01-04 19:35:15 +0300

    Create reverse_triangle.c

---
+++ final_practice/reverse_triangle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int i, j;
+
+    for (i = 5; i >= 1; i--)
+    {
+        for (j = 5; j >= i; j--)
+        {
+            printf("%d\t", j);
+        }
+        printf("\n");
+    }
+    
+    getchar();
+    return 0;
+}
